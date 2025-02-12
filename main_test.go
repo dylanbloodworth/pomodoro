8132a8dd89d33517d35b58c30f9cfc9b3ef1610c
Dylan Bloodworth <dylan.bloodworth@gmail.com>
2025-02-12 18:24:01 -0500

    Removes tests because I didn't know what I was doing

--- main_test.go
+++ main_test.go
@@ -1,17 +1,1 @@
 package main
-
-import (
-	"slices"
-	"testing"
-)
-
-func TestInitialModel(t *testing.T) {
-	got := InitialModel()
-
-	t.Run("testing initial model", func(t *testing.T) {
-		want := []string{"Buy carrots", "Buy celery", "Buy kohlrabi"}
-		if !slices.Equal(got.choices, want) {
-			t.Errorf("got %v, but wanted %v", got.choices, want)
-		}
-	})
-}
