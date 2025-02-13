e0082ce9af3ad66b688d297b94c5dd7939a24521
Dylan Bloodworth <dylan.bloodworth@gmail.com>
2025-02-13 18:41:33 -0500

    movign things around

---
+++ internal/usrinput/usrinput.go
@@ -0,0 +1,30 @@
+package usrinput
+
+import (
+	"fmt"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"log"
+)
+
+type model struct {
+	textInput textinput.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m model) View() string {
+	return ""
+}
+
+func InitialModel() model {
+	ti := textinput.New()
+
+	return model{textInput: ti}
+}
