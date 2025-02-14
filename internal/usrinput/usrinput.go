553f203aede7a4b0cf19032876c5bb853af911d9
Dylan Bloodworth <dylan.bloodworth@gmail.com>
2025-02-13 19:16:59 -0500

    Figure out a rough user input model that saves what the latest input was in a model field

--- internal/usrinput/usrinput.go
+++ internal/usrinput/usrinput.go
@@ -1,4 +1,4 @@
-package usrinput
+package main
 
 import (
 	"fmt"
@@ -9,22 +9,54 @@ import (
 
 type model struct {
 	textInput textinput.Model
+	usrInput  string
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return m, nil
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		//then look at the key that was pressed
+		switch msg.String() {
+		//If the key was enter
+		case "esc":
+			//Return the model and quit
+			return m, tea.Quit
+		case "enter":
+			m.usrInput = m.textInput.View()
+			return m, nil
+		}
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
-	return ""
+	form := fmt.Sprintf(
+		"What am I typing?, \n\n%s",
+		m.textInput.View(),
+	)
+	output := m.usrInput
+
+	return form + "\n\n" + output
 }
 
 func InitialModel() model {
 	ti := textinput.New()
+	ti.Focus()
 
 	return model{textInput: ti}
+}
+
+func main() {
+	p := tea.NewProgram(InitialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
